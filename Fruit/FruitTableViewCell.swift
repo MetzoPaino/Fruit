8cf36ea2c11ae0526d4df62bc36da2e0b4889067
William Robinson <williamjrobinson@me.com>
2018-03-07 19:46:05 +0000

    Deleted ViewController, created FruitTableViewCell

---
+++ Fruit/FruitTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  FruitTableViewCell.swift
+//  Fruit
+//
+//  Created by William Robinson on 07/03/2018.
+//  Copyright © 2018 William Robinson. All rights reserved.
+//
+
+import UIKit
+
+class FruitTableViewCell: UITableViewCell {
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+
+    //MARK: - Setup
+    func setup(fruit:Fruit) {
+
+        nameLabel.text = fruit.type.capitalized
+    }
+}
