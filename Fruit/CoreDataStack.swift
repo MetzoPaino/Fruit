5365ec7e239090cc75130560a9ae17f417baaf57
William Robinson <williamjrobinson@me.com>
2018-03-07 20:06:39 +0000

    Added a coreDataStack to get the persistent store

---
+++ Fruit/CoreDataStack.swift
@@ -0,0 +1,46 @@
+//
+//  CoreDataStack.swift
+//  ColorKeeper
+//
+//  Created by William Robinson on 12/01/2018.
+//  Copyright © 2018 William Robinson. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+
+    //MARK: - Variables
+    private let modelName: String
+
+    lazy var managedContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+
+    private lazy var storeContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    //MARK: - Life Cycle
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+
+    func saveContext () {
+        guard managedContext.hasChanges else { return }
+
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+}
