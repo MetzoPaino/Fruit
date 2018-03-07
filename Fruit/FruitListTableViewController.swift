9f05731f68c8263869d57bc5813de54e0a95f5f2
William Robinson <williamjrobinson@me.com>
2018-03-07 20:12:34 +0000

    FruitListTableViewController now shows data

--- Fruit/FruitListTableViewController.swift
+++ Fruit/FruitListTableViewController.swift
@@ -7,80 +7,66 @@
 //
 
 import UIKit
+import CoreData
 
-class FruitListTableViewController: UITableViewController {
+class FruitListTableViewController: UITableViewController  {
+
+    //MARK: - Variables
+    var coreDataStack: CoreDataStack!
+
+    lazy var fetchedResultsController: NSFetchedResultsController<Fruit> = {
+        let fetchRequest: NSFetchRequest<Fruit> = Fruit.fetchRequest()
+        let nameSort = NSSortDescriptor(key: #keyPath(Fruit.type), ascending: true)
+        fetchRequest.sortDescriptors = [nameSort]
+
+        let fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: coreDataStack.managedContext,
+            sectionNameKeyPath: nil,
+            cacheName: "Fruits")
+
+        return fetchedResultsController
+    }()
+
+    //MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch let error as NSError {
+            print("Fetching error: \(error), \(error.userInfo)")
+        }
+
+//        UITableView.appearance().separatorInset = .zero
+//         UITableView.appearance().tableFooterView = UIView()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        guard let fetchedObjects = fetchedResultsController.fetchedObjects else {
+            return 0
+        }
+        return fetchedObjects.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? FruitTableViewCell else {
+            return UITableViewCell()
+        }
+
+        let fruit = fetchedResultsController.object(at: indexPath)
+        cell.setup(fruit: fruit)
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -92,4 +78,49 @@ class FruitListTableViewController: UITableViewController {
     }
     */
 
+}
+
+// MARK: - NSFetchedResultsControllerDelegate
+
+extension FruitListTableViewController: NSFetchedResultsControllerDelegate {
+
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch (type) {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .automatic)
+            }
+            break;
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+            break;
+        case .update:
+            if
+                let indexPath = indexPath,
+                let cell = tableView.cellForRow(at: indexPath) as? FruitTableViewCell {
+                    let fruit = fetchedResultsController.object(at: indexPath)
+                    cell.setup(fruit: fruit)
+            }
+            break;
+        case .move:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+
+            if let newIndexPath = newIndexPath {
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+            break;
+        }
+    }
 }
