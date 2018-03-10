d81179cb436457fd98944d813e847ab4b67aeb53
William Robinson <williamjrobinson@me.com>
2018-03-10 15:22:40 +0000

    Created JSON helper for parsing

---
+++ Fruit/Helpers/JSONHelper.swift
@@ -0,0 +1,22 @@
+//
+//  JSONHelper.swift
+//  Fruit
+//
+//  Created by William Robinson on 10/03/2018.
+//  Copyright © 2018 William Robinson. All rights reserved.
+//
+
+import Foundation
+
+func parseJSON(data: Data) throws -> [[String: Any]] {
+
+    let topLevelKey = "fruit"
+    if
+        let jsonDictionary = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String: Any],
+        let jsonArray =  jsonDictionary[topLevelKey] as? [[String: Any]] {
+
+        return jsonArray
+    } else {
+        throw DownloadManagerError.jsonParser
+    }
+}
